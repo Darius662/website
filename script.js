d5b703cc2f820bd53444df9950322607036d394e
Darius662 <dariusjeleru@gmail.com>
2025-01-10 11:07:13 +0200

    test

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 // Add your JavaScript functionality here if needed.
 console.log("Welcome to Darius Jeleru's Professional Profile Website!");
+
+const toggleButton = document.getElementById("toggle-theme");
+
+toggleButton.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+});
